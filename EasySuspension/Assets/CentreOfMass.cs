a3f8e21bdbb911684c7050a7fc07bb911b9f6181
Radar-Games <154015@154015-SCT11837.gateway>
2017-09-13 18:52:57 +0800

    YEET

---
+++ EasySuspension/Assets/CentreOfMass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CentreOfMass : MonoBehaviour {
+
+	public Vector3 com;
+	public Rigidbody rb;
+
+	void Update () {
+		rb = GetComponent<Rigidbody> ();
+		rb.centerOfMass = com;
+	}
+}
