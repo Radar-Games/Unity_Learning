19f48b948b9012b6d697eb1e4961c48a5681b793
Radar-Games <154015@154015-SCT11837.local>
2017-09-15 13:34:15 +0800

    shit

--- EasySuspension2/Assets/InAirControl.cs
+++ EasySuspension2/Assets/InAirControl.cs
@@ -6,49 +6,84 @@ public class InAirControl : MonoBehaviour {
 
     public Rigidbody rb;
     public float speed;
+	public float speed2;
+	public float multi;
+	public Transform t;
+
+    public bool Assistance;
 
 	void Update ()
     {
         Vector3 com = rb.centerOfMass;
+        Vector3 newCom = new Vector3();
+        float rotationz = t.rotation.z * 30;
+        float rotationx = t.rotation.x * 30;
+
+        if (Assistance)
+        {
+            if (rotationz > 3)
+            {
+                newCom.z += speed2 * (rotationz * multi);
+            }
+
+            if (rotationz < -3)
+            {
+                newCom.z += -speed2 * (rotationz * multi);
+            }
+
+            if (rotationx > 3)
+            {
+                newCom.x += -speed2 * (rotationx * multi);
+            }
+
+            if (rotationx < -3)
+            {
+                newCom.x += speed2 * (rotationx * multi);
+            }
+        }
 
         bool i = false;
 
         if (Input.GetKey("t") && !Input.GetKey("g"))
         {
-            com.z = speed;
+            newCom.x = speed;
             i = true;
         }
 
         if (Input.GetKey("g") && !Input.GetKey("t"))
         {
-            com.z = -speed;
+            newCom.x = -speed;
             i = true;
         }
 
         if (Input.GetKey("f") && !Input.GetKey("h"))
         {
-            com.x = -speed;
+            newCom.z = speed;
             i = true;
         }
 
         if (Input.GetKey("h") && !Input.GetKey("f"))
         {
-            com.x = speed;
+            newCom.z = -speed;
             i = true;
         }
 
-        if (i == false)
-        {
-            com.x = 0;
-            com.z = 0;
-        }
-        else
-        {
-            i = false;
-        }
+        //if (i == false)
+        //{
+        //    newCom.x = 0;
+        //    newCom.z = 0;
+        //}
+        //else
+        //{
+        //    i = false;
+        //}
 
-        rb.centerOfMass = com;
+        com += newCom;
 
-        Debug.Log(com);
+        rb.AddTorque(com);
+
+        Debug.Log(rotationx);
+
+        //Debug.Log(com);
 	}
 }
